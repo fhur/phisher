4b8d9c56f9ad2ab64729dd2516c54babc4f2622f
fhur <fernando@authy.com>
2014-05-02 22:51:22 -0500

    added minimal specs to site_spec

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,8 @@
 class Site < ActiveRecord::Base
+
+  def phishy?
+    return rating < 0.5
+  end
+
 end
+
---
+++ spec/models/site_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Site do
+
+  let(:phishy_site) do
+    site = Site.new
+    site.url = "http://some.phishy-spamish-url.com"
+    site.rating = 0.01
+    return site
+  end
+
+  let(:safe_site) do
+    site = Site.new
+    site.url = "http://legiturl.com"
+    site.rating = 0.99
+    return site
+  end
+
+  describe '.phishy?' do
+
+    it 'should show phishy if the site rating is high' do
+      expect(phishy_site.phishy?).to be_truthy
+    end
+
+    it 'should not show phishy if the site rating is low' do
+      expect(safe_site.phishy?).to be_falsy
+    end
+
+    it 'should return return phishy if the site is blacklisted' do
+      safe_site.blacklisted = true
+      expect(safe_site.phishy?).to be_truthy
+    end
+  end
+
+
+end

c38d5ac8c4122718ef07a6b1587cb534f271a691
fhur <fernando@authy.com>
2014-12-04 15:08:08 -0500

    Created CachedDataSource

---
+++ lib/phisher/data_sources/cached_data_source.rb
@@ -0,0 +1,26 @@
+require 'daybreak'
+require 'phisher/data_source'
+
+class CachedDataSource < DataSource
+
+  attr_reader :cache
+
+  def initialize(store_location)
+    @cache = Daybreak::DB.new store_location
+    at_exit { @cache.close }
+  end
+
+  def get(url)
+    cleaned_url = clean(url)
+    if @cache.include? cleaned_url
+      return @cache[cleaned_url]
+    else
+      fetched = fetch(cleaned_url)
+      return @cache.set! cleaned_url, fetched
+    end
+  end
+
+  def fetch(url)
+    raise "Unimplemented method fetch(url)"
+  end
+end

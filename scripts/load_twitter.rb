f4d10f679cdaac981544d4d243ab15797a26b235
fhur <fernando@authy.com>
2014-12-05 14:57:15 -0500

    Created script to load the twitter data source

---
+++ scripts/load_twitter.rb
@@ -0,0 +1,24 @@
+require 'phisher/data_sources/twitter_data_source'
+
+twitter_key = ENV['TWITTER_KEY']
+twitter_secret = ENV['TWITTER_SECRET']
+
+[twitter_key, twitter_secret].each do |key|
+  raise "key is nil " if key == nil
+end
+
+@ds = TwitterDataSource.new key: twitter_key, secret: twitter_secret
+
+path = ARGV[0]
+
+puts "Loading #{path}"
+
+file = File.read(path)
+urls = file.split "\n"
+
+urls.each do |url|
+  print "."
+  @ds.get url
+end
+
+

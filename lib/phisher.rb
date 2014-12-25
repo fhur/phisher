1d397b053a5fe08f58360badaeab25037e1c9f33
fhur <fernando@authy.com>
2014-12-25 17:42:40 -0500

    add requires to lib/phisher

---
+++ lib/phisher.rb
@@ -0,0 +1,11 @@
+# The main class
+require 'phisher/phisher'
+
+# Include the tester
+require 'phisher/phisher_tester'
+
+# Include the list of algos
+require 'phisher/algos/search_engine_algo'
+require 'phisher/algos/duck_duck_go_algo'
+require 'phisher/algos/twitter_algo'
+

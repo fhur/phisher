8898ec614650ffccc43ac143ce738e7b68b92154
fhur <fernando@authy.com>
2014-12-25 17:41:06 -0500

    Created the WeightFunction and UniformWeightFunction classes

---
+++ lib/phisher/weight_function.rb
@@ -0,0 +1,18 @@
+class WeightFunction
+
+  def weight(algos, url)
+    raise "Unimplemented method: return the weigth of the given algos #{algos}"
+  end
+end
+
+# A weighting function that assigns the same weight to every Algo
+class UnformWeightFunction < WeightFunction
+
+  # Returns:
+  #   A Hash where the same weight is assigned uniformly to every algo
+  #   (i.e. the weight is 1/num_algos)
+  def weigth(algos, url)
+    weight = 1.to_f/algos.size # the same weight for each algo
+    return Hash[algos.zip([weight]*algos.size)]
+  end
+end

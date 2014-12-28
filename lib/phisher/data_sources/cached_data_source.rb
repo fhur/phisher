351c72f8b1b2b8a76ddfe81e06f60bc42de45b9a
fhur <fernando@authy.com>
2014-12-28 17:35:22 -0500

    added documentation to CachedDataSource

--- lib/phisher/data_sources/cached_data_source.rb
+++ lib/phisher/data_sources/cached_data_source.rb
@@ -1,6 +1,8 @@
 require 'daybreak'
 require 'phisher/data_source'
 
+# An implementation of a DataSource backed by a cache that stored results
+# in disk.
 class CachedDataSource < DataSource
 
   attr_reader :cache
@@ -10,6 +12,12 @@ class CachedDataSource < DataSource
     at_exit { @cache.close }
   end
 
+  # Returns:
+  #   {Array} Returns a data representation of the given url as a vector (Array)
+  #           This method is backed by a cache meaning that when DataSource#get is
+  #           first called for a given url it might consume network resources.
+  #           Afterwards the result is stored in a cache which means subsequent
+  #           calls are very efficient.
   def get(url)
     cleaned_url = clean(url)
     if @cache.include? cleaned_url
@@ -20,10 +28,21 @@ class CachedDataSource < DataSource
     end
   end
 
+  # Subclasses must implement this method.
+  # Given a url, this method should return the data that will be stored
+  # in the cache. You must return an an array of primitive data types.
+  #
+  # Arguments:
+  #   {string} url a url represented as a string
+  #
+  # Returns:
+  #   {Array} an array of numbers
   def fetch(url)
     raise "Unimplemented method fetch(url)"
   end
 
+  # Do some preprocessing on the url like removing protocol information so that
+  # for example http and https urls map to the same cached reference.
   # by default this method does nothing.
   def clean(url)
     url
